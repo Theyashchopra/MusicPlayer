c0715d67aecc58701748419758c87def8e21a495
yash chopra <yashchopra630@gmail.com>
2020-11-24 19:33:33 +0530

    yo

---
+++ app/src/main/java/com/plcoding/spotifycloneyt/data/entities/Song.kt
@@ -0,0 +1,9 @@
+package com.plcoding.spotifycloneyt.data.entities
+
+data class Song (
+    val mediaid: String = "",
+    val Title: String = "",
+    val subtitle: String = "",
+    val songurl: String = "",
+    val imageurl: String = ""
+)
